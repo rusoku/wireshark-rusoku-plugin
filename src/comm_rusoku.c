7860f10612083e1d996aaf6abd97350a0b594458
Gediminas Simaskis <gediminas@rusoku.com>
2025-01-10 13:06:07 +0200

    2025.01.10d1

--- src/comm_rusoku.c
+++ src/comm_rusoku.c
@@ -7,9 +7,10 @@
 
 
 static uint32_t hcanal;
-typedef uint32_t (*CanalOpen_fd)(const char *pConfigStr, unsigned long flags);
 
-CanalOpen_fd CanalOpen;
+typedef uint32_t (*CanalOpen_fp)(const char *pConfigStr, unsigned long flags);
+
+CanalOpen_fp CanalOpen;
 
 int16_t comm_init_rusoku(void *handler, char *error_code){
 
@@ -17,11 +18,14 @@ int16_t comm_init_rusoku(void *handler, char *error_code){
         return COMM_LIB_ERROR;
     }
 
-#ifdef WIN32
+#if defined __WIN32__ || defined __WIN64__
     handler = dlopen("canal.dll", RTLD_NOW);
-#elif
+#elif defined __APPLE__
     handler = dlopen("uvcantou.dylib", RTLD_NOW);
+#else
+    handler = dlopen("uvcantou.so", RTLD_NOW);
 #endif
+
     if (handler == NULL) {
         if (error_code != NULL) {
             error_code = dlerror();
