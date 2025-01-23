3641e4edf42007959cb79fdb1e9919b9051d077b
Gediminas Simanskis <gediminas@rusoku.com>
2025-01-23 22:07:04 +0200

    20250123n1

--- main/src/control_packet.c
+++ main/src/control_packet.c
@@ -8,32 +8,43 @@
 void *ctrl_read_thread(void *ptr) {
     FILE *ctrl_in;
     ctrl_in = (FILE *) ptr;
-    struct CONTROL_PACKET_HEADERS *ctrl_packet_header;
     uint32_t payload_length;
     uint32_t read_len, file_size;;
-    uint8_t buff[1024];
-
-    ctrl_packet_header = (struct CONTROL_PACKET_HEADERS *) buff;
+    struct CONTROL_PACKET_HEADERS ctrl_packet = {0};
+    uint8_t buff[512];
 
     while (onCapture) {
-        read_len = fread(buff, 1, 6, ctrl_in);
+        fread(&ctrl_packet, sizeof(struct CONTROL_PACKET_HEADERS), 1, ctrl_in);
 
         DebugPrintf("************** CONTROL PACKET **********************");
-        DebugPrintf("control:Sync Pipe Indication=%C\n", ctrl_packet_header->sync);
-        DebugPrintf("control:Len2=%d\n", ctrl_packet_header->len[2]);
-        DebugPrintf("control:Len1=%d\n", ctrl_packet_header->len[1]);
-        DebugPrintf("control:Len0=%d\n", ctrl_packet_header->len[0]);
-        DebugPrintf("control:CtlrNumber=%d\n", ctrl_packet_header->ctrl_number);
-        DebugPrintf("control:Command=%d\n", ctrl_packet_header->command);
+        DebugPrintf("control:Sync Pipe Indication=%C\n", ctrl_packet.sync);
+        DebugPrintf("control:Len2=%d\n", ctrl_packet.len[2]);
+        DebugPrintf("control:Len1=%d\n", ctrl_packet.len[1]);
+        DebugPrintf("control:Len0=%d\n", ctrl_packet.len[0]);
+        DebugPrintf("control:CtlrNumber=%d\n", ctrl_packet.ctrl_number);
+        DebugPrintf("control:Command=%d\n", ctrl_packet.command);
 
         //payload_length = (ctrl_packet_header->len[1] << 16 | ctrl_packet_header->len[1] << 8 | ctrl_packet_header->len[2]);
-        payload_length = ctrl_packet_header->len[2];
+        payload_length = ctrl_packet.len[2];
         payload_length -= 2;
+
+        switch (ctrl_packet.ctrl_number) {
+            case 1:
+
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            default:
+        }
 
         if (payload_length != 0) {
             read_len = fread(buff, 1, payload_length, ctrl_in);
-            //DebugPrintf("control:data=%d %d %d%", buff[0], buff[1], buff[2], buff[3]);
-            DebugPrintf("control:data=%d", buff[0]);
         }
     }
 
@@ -44,9 +55,19 @@ void *ctrl_read_thread(void *ptr) {
 void *ctrl_send_thread(void *ptr) {
     FILE *ctrl_out;
     ctrl_out = (FILE *) ptr;
+    //const uint8_t buff[] = "123456789";
 
+    /*
+    struct CONTROL_PACKET_HEADERS ctrl_packet = {0};
+    ctrl_packet.sync = 'T';
+    ctrl_packet.len[2] = 10;
+    ctrl_packet.ctrl_number = 0;
+    ctrl_packet.command = 6;
 
+    fwrite(&ctrl_packet, sizeof(struct CONTROL_PACKET_HEADERS), 1, ctrl_out);
+    fwrite(&ctrl_packet, 10, 1, ctrl_out);
 
     DebugPrintf("control:RETURN");
+    */
     return NULL;
 }
